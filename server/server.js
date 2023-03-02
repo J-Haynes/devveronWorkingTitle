b99873108f5181b544e79c6012638c1b37187780
patnz <114084721+patnz@users.noreply.github.com>
2023-03-02 18:08:39 +1300

    server is REALLY up and running

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const server = express()
+
+// const townRouter = require('./townRouter')
+// const townRouter = require('./countryRouter') < --- assuming the names of these routers
+
+// Server config
+
+const publicFolder = __dirname + '/public'
+server.use(express.static(publicFolder))
+
+// Handlebars config
+
+server.engine('hbs', hbs.engine({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+server.set('views', __dirname + '/views')
+
+const testMessage = 'HELLO THERE!'
+
+server.get('/', (req, res) => {
+  res.send(testMessage)
+})
+
+// Export !
+
+module.exports = server
